db0d51338105a00532814cdd3d8f2e2dbcb64b27
etitolo <etitolo@wsgc.com>
2016-05-10 15:13:09 -0700

    refactor jQuery out of JS

--- js/main.js
+++ js/main.js
@@ -3,20 +3,31 @@ function tabs() {
     var tabNumber = document.querySelectorAll(".tabs a");
     var tabContent = document.querySelectorAll(".tabContent div");
 
-    for (var i=0; i < tabNumber.length; i++) {
-        tabContent[i].classList.add("tab-" + i);
+    for (i=0; i < tabNumber.length; i++) {
+        var el = tabNumber[i];
         tabNumber[i].setAttribute("data-tab", "tab-" + i);
+        el.addEventListener("click", toggleCurrentTab, false);
+    }
+    for (i=0; i < tabContent.length; i++) {
+        tabContent[i].classList.add("tab-" + i);
     }
     
-    $('.tabs a').click(function(){
-        var tabId = $(this).attr('data-tab'); 
+    function toggleCurrentTab(e) {
+        for (i=0; i < tabNumber.length; i++) {
+            var removeCurrent = tabNumber[i];
+            removeCurrent.classList.remove("current");
+        }
+        for (i=0; i < tabContent.length; i++) {
+            var removeCurrentContent = tabContent[i];
+            removeCurrentContent.classList.remove("current");
+        }
 
-        $('.tabs a').removeClass('current');
-        $('.tabContent div').removeClass('current');
-
-        $(this).addClass('current');
+        var tabId = $(this).attr('data-tab');
         $("."+tabId).addClass('current');
-    }); 
+        
+        e.currentTarget.classList.add("current");
+    }
 }
 tabs();
+
 
